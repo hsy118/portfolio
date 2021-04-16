5387bca33163473645f72ba838613f9cd31ae80d
hsy118 <hsy118@hotmail.co.kr>
2021-04-17 00:05:57 +0900

    handle scrolling event with vanilla js

---
+++ main.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+    if(window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null) {
+        return;
+    }
+    scrollIntoView(link)
+})
+
+// Handle scrolling when clicking on the 'contact me' btn
+
+const contactBtn = document.querySelector('.home__contact')
+
+contactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact')
+})
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container')
+const homeHeight = home.getBoundingClientRect().height
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+})
+
+
+
+
+
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'})
+}
